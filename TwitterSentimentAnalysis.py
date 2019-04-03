7697febe2229f3ce9c0e266fe8832de8e284b180
Asmin75 <asmin7@gmail.com>
2019-04-03 13:22:46 +0545

    function to build test

--- TwitterSentimentAnalysis.py
+++ TwitterSentimentAnalysis.py
@@ -9,29 +9,29 @@ twitter_api = twitter.Api(consumer_key='56YkT1Dw8FzWZgveLffz2eP1p',
 # test authentication
 print(twitter_api.VerifyCredentials())
 
-#
-# # ------------------------------------------------------------------------
-#
-# def buildTestSet(search_keyword):
-#     try:
-#         tweets_fetched = twitter_api.GetSearch(search_keyword, count=100)
-#
-#         print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
-#
-#         return [{"text": status.text, "label": None} for status in tweets_fetched]
-#     except:
-#         print("Unfortunately, something went wrong..")
-#         return None
-#
-#
-# # ------------------------------------------------------------------------
-#
-# search_term = input("Enter a search keyword: ")
-# testDataSet = buildTestSet(search_term)
-#
-# print(testDataSet[0:4])
-#
-#
+
+# ------------------------------------------------------------------------
+
+def buildTestSet(search_keyword):
+    try:
+        tweets_fetched = twitter_api.GetSearch(search_keyword, count=100)
+
+        print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+
+        return [{"text": status.text, "label": None} for status in tweets_fetched]
+    except:
+        print("Unfortunately, something went wrong..")
+        return None
+
+
+# ------------------------------------------------------------------------
+
+search_term = input("Enter a search keyword: ")
+testDataSet = buildTestSet(search_term)
+
+print(testDataSet[0:4])
+
+
 # # ------------------------------------------------------------------------
 #
 # def buildTrainingSet(corpusFile, tweetDataFile):
