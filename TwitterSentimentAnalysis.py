50616ee9a28d87eb85e1498b8766fc73f22fd134
Asmin75 <asmin7@gmail.com>
2019-04-29 23:07:18 +0545

    Twitter api part

--- TwitterSentimentAnalysis.py
+++ TwitterSentimentAnalysis.py
@@ -1,13 +1,7 @@
-import twitter
-
-# initialize api instance
-twitter_api = twitter.Api(consumer_key='56YkT1Dw8FzWZgveLffz2eP1p',
-                          consumer_secret='nAsmznOH2Fo9ha1yii1YKtWpL8MfbQrARIVBblauqMCToDJvrI',
-                          access_token_key='1110107966919925760-yMbsWLYmnSLvbhqwDcBG1ZXlQOEnjo',
-                          access_token_secret='LaEFIWRr7eLnMCI1KLbKW3LaEkhw7l56QuRTEnGqOdwA5')
+from TwitterApi import twitterApi
 
 # test authentication
-print(twitter_api.VerifyCredentials())
+print(twitterApi.twitter_api.VerifyCredentials())
 
 
 """Building Test Set Function
@@ -16,7 +10,7 @@ Tweets are downloaded  for the search term
 
 def buildTestSet(search_keyword):
     try:
-        tweets_fetched = twitter_api.GetSearch(search_keyword, count=100)
+        tweets_fetched = twitterApi.twitter_api.GetSearch(search_keyword, count=100)
 
         print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
 
